2fc87027ae5d2885701eef03b8c2a1fde4c9314a
moin <moinkhan603@gmail.com>
2020-07-12 05:32:35 +0500

    drawer added

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nomi/CRUD.dart';
 import 'package:nomi/profile.dart';
@@ -40,32 +42,50 @@ class _WelcomeState extends State<Welcome> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: FloatingNavbar(
-        selectedBackgroundColor: Colors.white,
-        iconSize: 30,
-        fontSize: 18,
-        backgroundColor: Colors.blue,
-        onTap: (int val) => setState(() => _index = val),
-        currentIndex: _index,
-        items: [
+    return WillPopScope(
+      onWillPop: willpop,
+      child: Scaffold(
+
+        bottomNavigationBar: FloatingNavbar(
+          selectedBackgroundColor: Colors.white,
+          iconSize: 25,
+          fontSize: 18,
+          backgroundColor: Colors.blue,
+          onTap: (int val) => setState(() => _index = val),
+          currentIndex: _index,
+          items: [
 //          FloatingNavbarItem(icon:Image.asset("person_icon.png")  , title: 'Home'),
 //          FloatingNavbarItem(icon:Image.asset("person_icon.png")  , title: 'Home'),
 //          FloatingNavbarItem(icon:Image.asset("person_icon.png")  , title: 'Home'),
 
-          FloatingNavbarItem(
-            icon: FaIcon(
-              FontAwesomeIcons.table,
-            ).icon,
-            title: 'Read',
-          ),
-          FloatingNavbarItem(
-              icon: FaIcon(FontAwesomeIcons.edit).icon, title: 'Write'),
-          FloatingNavbarItem(
-              icon: FaIcon(FontAwesomeIcons.user).icon, title: 'Profile'),
-        ],
+            FloatingNavbarItem(
+              icon: FaIcon(
+                FontAwesomeIcons.table,
+              ).icon,
+              title: 'Read',
+            ),
+            FloatingNavbarItem(
+                icon: FaIcon(FontAwesomeIcons.edit).icon, title: 'Write'),
+            FloatingNavbarItem(
+                icon: FaIcon(FontAwesomeIcons.user).icon, title: 'Profile'),
+          ],
+        ),
+        body: _children[_index],
       ),
-      body: _children[_index],
     );
+  }
+
+  Future<bool> willpop() {
+
+    FirebaseAuth.instance.currentUser().then((firebaseUser){
+      if(firebaseUser == null)
+      {
+        //signed out
+      }
+      else if(firebaseUser!=null){
+        //signed in
+      SystemNavigator.pop();
+      }
+    });
   }
 }
