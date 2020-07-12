cf6d811749e2072ef5cb0d4999ebf63be0a2acc6
moin <moinkhan603@gmail.com>
2020-07-12 18:02:15 +0500

    vcf card enabled

--- lib/vcard.dart
+++ lib/vcard.dart
@@ -73,18 +73,6 @@ Sharefile()async{
 }
 
 
-Future<void> initDownloadsDirectoryState() async {
-
-
-  // If the widget was removed from the tree while the asynchronous platform
-  // message was in flight, we want to discard the reply rather than calling
-  // setState to update our non-existent appearance.
-//  if (!mounted) return;
-//
-//  setState(() {
-//    _downloadsDirectory = downloadsDirectory;
-//  });
-}
 
 
 }
